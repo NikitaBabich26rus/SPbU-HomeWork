2846015d8a50d0b76e6e8d0e54df1f80dfeb021d
NikitaBabich26rus <mishamatreshkin@mail.ru>
2019-10-08 00:15:48 +0300

    4.2

---
+++ HomeWork4/4.2/4.2/qsort.h
@@ -0,0 +1,34 @@
+#pragma once
+
+void qsort(int countLeft, int countRight, int array[])
+{
+	int value = array[(countLeft + countRight) / 2];
+	for (int i = countLeft, j = countRight; i < j;)
+	{
+		if (array[i] > array[j])
+		{
+			int const helpValue = array[i];
+			array[i] = array[j];
+			array[j] = helpValue;
+		}
+		if (array[i] < value)
+		{
+			i++;
+		}
+		else
+		{
+			j--;
+		}
+		if (i == j)
+		{
+			if (i - countLeft + 1 >= 3)
+			{
+				qsort(countLeft, i, array);
+			}
+			if (countRight - j + 1 >= 3)
+			{
+				qsort(j + 1, countRight, array);
+			}
+		}
+	}
+}

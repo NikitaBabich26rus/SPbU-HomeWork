810b74260cf4015970bdff873ba4abf1142005c4
NikitaBabich26rus <mishamatreshkin@mail.ru>
2019-11-25 01:47:16 +0300

    - Добавлена функция удаления списка

---
+++ HomeWork7/7.3(2)/7.3(2)/List.cpp
@@ -0,0 +1,169 @@
+#include "List.h"
+#include <string.h>
+#include <stdio.h>
+
+struct List
+{
+	ListElement* head = nullptr;
+	ListElement* tail = nullptr;
+	int size = 0;
+};
+
+struct ListElement
+{
+	char mainString[sizeOfString]{};
+	char sideString[sizeOfString]{};
+	ListElement* next = nullptr;
+	ListElement* previos = nullptr;
+};
+
+List* createList()
+{
+	return new List;
+}
+
+bool empty(List* list)
+{
+	return list->head == nullptr;
+}
+
+int takeListSize(List* list)
+{
+	return list->size;
+}
+
+void push(List* list, char mainString[], char sideString[])
+{
+	if (list->head == nullptr)
+	{
+		list->head = new ListElement;
+		strcpy(list->head->mainString, mainString);
+		strcpy(list->head->sideString, sideString);
+		list->tail = list->head;
+		list->size = list->size + 1;
+		return;
+	}
+	list->tail->next = new ListElement;
+	list->tail->next->previos = list->tail;
+	list->tail = list->tail->next;
+	strcpy(list->tail->mainString, mainString);
+	strcpy(list->tail->sideString, sideString);
+	list->size = list->size + 1;
+}
+
+void output(List* list)
+{
+	ListElement* helpElement = list->head;
+	for (;helpElement != nullptr;)
+	{
+		printf("%s ", helpElement->mainString);
+		printf("%s\n", helpElement->sideString);
+		helpElement = helpElement->next;
+	}
+}
+
+void createNewRightList(List* list, List* rightList, int size)
+{
+	ListElement* helpElement = list->tail;
+	for (int i = size - 1; i >= size / 2; i--)
+	{
+		char mainString[sizeOfString]{};
+		char sideString[sizeOfString]{};
+		strcpy(mainString, helpElement->mainString);
+		strcpy(sideString, helpElement->sideString);
+		push(rightList, mainString, sideString);
+		helpElement = helpElement->previos;
+	}
+}
+
+void createNewLeftList(List* list, List* leftList, int size)
+{
+	ListElement* helpElement = list->head;
+	for (int i = 0; i < size / 2; i++)
+	{
+		char mainString[sizeOfString]{};
+		char sideString[sizeOfString]{};
+		strcpy(mainString, helpElement->mainString);
+		strcpy(sideString, helpElement->sideString);
+		push(leftList, mainString, sideString);
+		helpElement = helpElement->next;
+	}
+}
+
+void deleteElement(List* list)
+{
+	if (list->head->next == nullptr)
+	{
+		delete list->head;
+		list->head = nullptr;
+		list->tail = nullptr;
+		list->size = 0;
+		return;
+	}
+	ListElement* helpElement = list->head->next;
+	delete list->head;
+	list->head = helpElement;
+	list->size = list->size - 1;
+	list->head->previos = nullptr;
+}
+
+void pushToNewList(List* list, List* leftList, List* rightList)
+{
+	char mainString[sizeOfString]{};
+	char sideString[sizeOfString]{};
+	if (strcmp(leftList->head->mainString, rightList->head->mainString) >= 0)
+	{
+		strcpy(mainString, rightList->head->mainString);
+		strcpy(sideString, rightList->head->sideString);
+		push(list, mainString, sideString);
+		deleteElement(rightList);
+		return;
+	}
+	strcpy(mainString, leftList->head->mainString);
+	strcpy(sideString, leftList->head->sideString);
+	push(list, mainString, sideString);
+	deleteElement(leftList);
+}
+
+void transferLastValues(List* oldList, List* newList)
+{
+	ListElement* helpElement = oldList->head;
+	while (helpElement != nullptr)
+	{
+		char mainString[sizeOfString]{};
+		char sideString[sizeOfString]{};
+		strcpy(mainString, helpElement->mainString);
+		strcpy(sideString, helpElement->sideString);
+		push(newList, mainString, sideString);
+		helpElement = helpElement->next;
+	}
+}
+
+bool checkSort(List* list)
+{
+	ListElement* firstElement = list->head;
+	ListElement* secondElement = nullptr;
+	for (;firstElement->next != nullptr;)
+	{
+		secondElement = firstElement;
+		firstElement = firstElement->next;
+		if (strcmp(secondElement->mainString, firstElement->mainString) > 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void deleteList(List* list)
+{
+	while (list->head->next != nullptr)
+	{
+		ListElement* helpElement = list->head->next;
+		delete list->head;
+		list->head = helpElement;
+		helpElement->previos = nullptr;
+	}
+	delete list->head;
+	delete list;
+}
---
+++ HomeWork7/7.3(2)/7.3(2)/List.h
@@ -0,0 +1,43 @@
+#pragma once
+
+// Размер номера телефона и имени
+const int sizeOfString = 50;
+
+// Список
+struct List;
+
+// Элемент списка
+struct ListElement;
+
+// Создать список
+List* createList();
+
+// Добавить элемент в список
+void push(List* list, char mainString[], char sideString[]);
+
+// Вывести список
+void output(List* list);
+
+// Создать новый список из левой части старого списка
+void createNewLeftList(List* list, List* leftList, int size);
+
+// Создать новый список из правой части старого списка
+void createNewRightList(List* list, List* rightList, int size);
+
+// Возвращает размер списка
+int takeListSize(List* list);
+
+// Проверка списка на пустоту 
+bool empty(List* list);
+
+// Передать последние значение из левого или правого списка в новый(во время слияния)
+void transferLastValues(List* oldList, List* newList);
+
+// Добавить элемент в новый список(во время слияния) 
+void pushToNewList(List* list, List* leftList, List* rightList);
+
+// Проверка на списка на отсоритрованность по неубыванию
+bool checkSort(List* list);
+
+// Удаление списка
+void deleteList(List* list);

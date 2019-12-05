8fa613e6fb67f758429452357d18b2f3ff217236
NikitaBabich26rus <mishamatreshkin@mail.ru>
2019-12-05 12:05:51 +0300

    - Исправлены недочеты - Устранена утечка памяти

---
+++ HomeWork7/7.3(3)/7.3(3)/7.3(3).cpp
@@ -0,0 +1,59 @@
+﻿#include "List.h"
+#include <stdio.h>
+#include <string.h>
+#include "MergeSort.h"
+#include <locale.h>
+
+bool test()
+{
+	FILE* file = fopen("Test.txt", "r");
+	List* list = createList();
+	while (!feof(file))
+	{
+		char name[sizeOfString]{};
+		char number[sizeOfString]{};
+		fscanf(file, "%s", name);
+		fscanf(file, "%s", number);
+		push(list, name, number);
+	}
+	fclose(file);
+	list = mergeSort(list);
+	bool result = checkSort(list);
+	deleteList(list);
+	return result;
+}
+
+int main()
+{
+	if (!test())
+	{
+		return -1;
+	}
+	setlocale(LC_ALL, "Rus");
+	int choice = 0;
+	printf("Для сортировки по имени введите 1, по номеру ввведите 0\n");
+	scanf("%d", &choice);
+	List* list = createList();
+	FILE* file = fopen("Input.txt", "r");
+	while (!feof(file))
+	{
+		char name[sizeOfString]{};
+		char number[sizeOfString]{};
+		fscanf(file, "%s", name);
+		fscanf(file, "%s", number);
+		if (choice == 1)
+		{
+			push(list, name, number);
+		}
+		else
+		{
+			push(list, number, name);
+		}
+	}
+	fclose(file);
+	list = mergeSort(list);
+	output(list);
+	deleteList(list);
+	return 0;
+}
+
---
+++ HomeWork7/7.3(3)/7.3(3)/MergeSort.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Список
+struct List;
+
+// Функция слияния
+List* merge(List* leftList, List* rightList);
+
+// Сортировка слиянием
+List* mergeSort(List* list);

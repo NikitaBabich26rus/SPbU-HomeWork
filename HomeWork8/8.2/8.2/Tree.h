7b3019d3dfac9ad2dcee1e68ac7d9ee974cdb00f
NikitaBabich26rus <mishamatreshkin@mail.ru>
2019-12-21 16:11:49 +0300

    - Добавлены комментарии

--- HomeWork8/8.2/8.2/Tree.h
+++ HomeWork8/8.2/8.2/Tree.h
@@ -1,15 +1,22 @@
 #pragma once
 
+// Дерево разбора арифметического выражения
 struct Tree;
 
+// Создать дерево разбора арифметического выражения
 Tree* createTree();
 
+// Построить дерево разбора арифметического выражения
 void buildTree(Tree* tree, char string[]);
 
+// Вывести дерево разбора арифметического выражения
 void outputTree(Tree* tree);
 
+// Посчитать результат в дереве разбора арифметического выражения
 void toCountResult(Tree* tree);
 
+// Получить значение из корня дерева
 int getValueOfRoot(Tree* tree);
 
+// Удалить дерево разбора арифметического выражения
 void deleteTree(Tree* tree);

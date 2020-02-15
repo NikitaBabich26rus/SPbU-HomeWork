fccbce272b3baa0f5ab3a58f6a38e010e8881a7e
NikitaBabich26rus <mishamatreshkin@mail.ru>
2020-02-15 17:58:11 +0300

    2 Semester : 1.3 - Первая версия

---
+++ 2sem.HomeWork1/2.1.3/2.1.3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2._1._3
+{
+    class Program
+    {
+        private static int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+            }
+            return array;
+        }
+        private static void OutputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива : ");
+            int sizeOfArray = int.Parse(Console.ReadLine());
+            int[] array = new int[sizeOfArray];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Введите " + (i + 1) + " элемент массива : ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            array = BubbleSort(array);
+            Console.Write("Отсортированный массив : ");
+            OutputArray(array);
+        }
+    }
+}

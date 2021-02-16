016c9741d8ce8737734bb86e216afb28ce37d445
NikitaBabich26rus <mishamatreshkin@mail.ru>
2021-02-16 20:41:49 +0300

    - Factorial

---
+++ Semester4/Homework1/Task2/Task2/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let getTheFactorialOfNumber (value) =
+    let rec factorial currentValue step =
+        if (value = step)
+            then Some(currentValue);
+        else
+            factorial (currentValue * (step + 1)) (step + 1)
+    if (value < 0)
+        then None
+    elif (value = 0)
+        then Some(0)
+    else
+        factorial 1 1
+
+
+[<EntryPoint>]
+let main argv =
+    let answer = getTheFactorialOfNumber 8
+    printfn "%A" answer
+    0 

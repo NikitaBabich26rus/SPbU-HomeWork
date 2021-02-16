93d74db22aed39a1cd4523f5a1a130c25944aef9
NikitaBabich26rus <mishamatreshkin@mail.ru>
2021-02-16 17:02:36 +0300

    - Fibonacci

---
+++ Semester4/Homework1/Task1/Task1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let getFibonacciValue fibonacciNumber =
+    let rec fibonacci value1 value2 step =
+        if (fibonacciNumber = 1)
+            then Some(value1)
+        elif (step = fibonacciNumber)
+            then Some(value2)
+        else
+            fibonacci (value2) (value2 + value1) (step + 1)
+    if (fibonacciNumber < 1)
+        then None
+    else
+        fibonacci 0 1 1
+
+[<EntryPoint>]
+let main argv =
+    let answer = getFibonacciValue 30
+    printfn "%A" answer
+    0

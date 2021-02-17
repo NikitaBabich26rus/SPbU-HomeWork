609018ee58b9e3eb5c9e98cda7727b0b12455169
NikitaBabich26rus <mishamatreshkin@mail.ru>
2021-02-17 17:57:26 +0300

    - Fixed code style errors

--- Semester4/Homework1/Task1/Task1/Program.fs
+++ Semester4/Homework1/Task1/Task1/Program.fs
@@ -2,19 +2,19 @@
 
 let getFibonacciValue fibonacciNumber =
     let rec fibonacci value1 value2 step =
-        if (fibonacciNumber = 1)
-            then Some(value1)
-        elif (step = fibonacciNumber)
-            then Some(value2)
-        else
-            fibonacci (value2) (value2 + value1) (step + 1)
-    if (fibonacciNumber < 1)
-        then None
-    else
+        if step = fibonacciNumber then
+            Some(value2)
+        else 
+            fibonacci value2 (value2 + value1) (step + 1)
+    if fibonacciNumber < 1 then 
+        None
+    elif fibonacciNumber = 1 then
+        Some(0)
+    else 
         fibonacci 0 1 1
 
 [<EntryPoint>]
 let main argv =
-    let answer = getFibonacciValue 30
+    let answer = getFibonacciValue 15
     printfn "%A" answer
     0

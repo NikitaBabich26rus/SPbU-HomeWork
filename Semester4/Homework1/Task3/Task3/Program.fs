be28c52e58fc5baf7be2feebca31db2dc42a3472
NikitaBabich26rus <mishamatreshkin@mail.ru>
2021-02-17 23:23:53 +0300

    - Exponentiation

---
+++ Semester4/Homework1/Task3/Task3/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let getExponentListOfTwo n m =
+    let rec exponentiation index list = 
+        if index = m then
+            Some(List.append list [pown 2 index])
+        else
+            exponentiation (index + 1) (List.append list [pown 2 index])
+
+    if n < 0 || m < 0 then
+        None
+    elif n = 0 && m = 0 then
+        Some([1])
+    else
+        exponentiation n ([])
+
+[<EntryPoint>]
+let main argv =
+    let answer = getExponentListOfTwo 7 12
+    printfn "%A" answer
+    0

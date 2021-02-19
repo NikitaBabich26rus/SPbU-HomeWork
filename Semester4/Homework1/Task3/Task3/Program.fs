98cb46d3522b3425333ada5fda6a40624540c014
NikitaBabich26rus <mishamatreshkin@mail.ru>
2021-02-19 11:26:06 +0300

    - Fixed bugs

--- Semester4/Homework1/Task3/Task3/Program.fs
+++ Semester4/Homework1/Task3/Task3/Program.fs
@@ -1,21 +1,28 @@
 ﻿open System
 
-let getExponentListOfTwo n m =
-    let rec exponentiation index list = 
-        if index = m then
-            Some(List.append list [pown 2 index])
-        else
-            exponentiation (index + 1) (List.append list [pown 2 index])
+let listReverse list = 
+    let rec reverse currentList newList =
+        if currentList = [] then
+            newList
+        else 
+            reverse (List.tail currentList) (List.head currentList :: newList)
+    reverse list []
 
+let getExponentListOfTwo n m =
+    let rec exponentiation index list currentValue = 
+        if index = m then
+            Some(listReverse (currentValue :: list))
+        else
+            exponentiation (index + 1) (currentValue :: list) (currentValue * 2)
     if n < 0 || m < 0 then
         None
     elif n = 0 && m = 0 then
         Some([1])
     else
-        exponentiation n ([])
+        exponentiation n ([]) (pown 2 n)
 
 [<EntryPoint>]
 let main argv =
-    let answer = getExponentListOfTwo 7 12
+    let answer = getExponentListOfTwo 1 6
     printfn "%A" answer
     0

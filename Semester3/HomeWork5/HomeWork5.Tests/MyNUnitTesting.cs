b1a556ba9d3586a5edb16c31f46e82a8a78b11e1
NikitaBabich26rus <mishamatreshkin@mail.ru>
2021-02-22 22:27:51 +0300

    - Edited Tests

--- Semester3/HomeWork5/HomeWork5.Tests/MyNUnitTesting.cs
+++ Semester3/HomeWork5/HomeWork5.Tests/MyNUnitTesting.cs
@@ -15,8 +15,11 @@ namespace HomeWork5.Tests
             tests = new MyNUnit("../../../../ExceptionTests");
             tests.ClassQueue.TryDequeue(out var info);
             var infoArray = info.ToArray();
-            Assert.IsTrue(IsEqual(infoArray[0], "ExceptionTest1", "Passed", "Exception has been thrown by the target of an invocation."));
-            Assert.IsTrue(IsEqual(infoArray[1], "ExceptionTest2", "Passed", "Exception has been thrown by the target of an invocation."));
+            var answer1 = IsEqual(infoArray[0], "ExceptionTest1", "Passed", "Exception has been thrown by the target of an invocation.")
+                && IsEqual(infoArray[1], "ExceptionTest2", "Passed", "Exception has been thrown by the target of an invocation.");
+            var answer2 = IsEqual(infoArray[1], "ExceptionTest1", "Passed", "Exception has been thrown by the target of an invocation.")
+                && IsEqual(infoArray[0], "ExceptionTest2", "Passed", "Exception has been thrown by the target of an invocation.");
+            Assert.IsTrue(answer1 || answer2);
         }
 
         [Test]
@@ -25,18 +28,22 @@ namespace HomeWork5.Tests
             tests = new MyNUnit("../../../../FailedTests");
             tests.ClassQueue.TryDequeue(out var info);
             var infoArray = info.ToArray();
-            Assert.IsTrue(IsEqual(infoArray[0], "FailedTest1", "Failed", "Exception has been thrown by the target of an invocation."));
-            Assert.IsTrue(IsEqual(infoArray[1], "FailedTest2", "Failed", "Exception has been thrown by the target of an invocation."));
+            var answer1 = IsEqual(infoArray[0], "FailedTest1", "Failed", "Exception has been thrown by the target of an invocation.")
+                && IsEqual(infoArray[1], "FailedTest2", "Failed", "Exception has been thrown by the target of an invocation.");
+            var answer2 = IsEqual(infoArray[1], "FailedTest1", "Failed", "Exception has been thrown by the target of an invocation.")
+                && IsEqual(infoArray[0], "FailedTest2", "Failed", "Exception has been thrown by the target of an invocation.");
+            Assert.IsTrue(answer1 || answer2);
         }
 
         [Test]
         public void PassedTests()
-        {
+        { 
             tests = new MyNUnit("../../../../PassedTests");
             tests.ClassQueue.TryDequeue(out var info);
             var infoArray = info.ToArray();
-            Assert.IsTrue(IsEqual(infoArray[0], "SucceededTest1", "Passed", null));
-            Assert.IsTrue(IsEqual(infoArray[1], "SucceededTest2", "Passed", null));
+            var answer1 = IsEqual(infoArray[0], "SucceededTest1", "Passed", null) && IsEqual(infoArray[1], "SucceededTest2", "Passed", null);
+            var answer2 = IsEqual(infoArray[0], "SucceededTest2", "Passed", null) && IsEqual(infoArray[1], "SucceededTest1", "Passed", null);
+            Assert.IsTrue(answer1 || answer2);
         }
     }
 }

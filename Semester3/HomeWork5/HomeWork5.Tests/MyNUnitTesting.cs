fe46b5b95279859d5d812bb11716e33957495564
Nikita26rus <mishamatreshkin@mailru>
2021-04-18 21:12:58 +0300

    - Changed

--- Semester3/HomeWork5/HomeWork5.Tests/MyNUnitTesting.cs
+++ Semester3/HomeWork5/HomeWork5.Tests/MyNUnitTesting.cs
@@ -10,19 +10,6 @@ namespace HomeWork5.Tests
             testInfo.Name == name && testInfo.Result == result && testInfo.IgnoreReason == ignoreReason;
 
         [Test]
-        public void ExceptionTests()
-        {
-            tests = new MyNUnit("../../../../ExceptionTests");
-            tests.ClassQueue.TryDequeue(out var info);
-            var infoArray = info.ToArray();
-            var answer1 = IsEqual(infoArray[0], "ExceptionTest1", "Passed", "Exception has been thrown by the target of an invocation.")
-                && IsEqual(infoArray[1], "ExceptionTest2", "Passed", "Exception has been thrown by the target of an invocation.");
-            var answer2 = IsEqual(infoArray[1], "ExceptionTest1", "Passed", "Exception has been thrown by the target of an invocation.")
-                && IsEqual(infoArray[0], "ExceptionTest2", "Passed", "Exception has been thrown by the target of an invocation.");
-            Assert.IsTrue(answer1 || answer2);
-        }
-
-        [Test]
         public void FailedTests()
         {
             tests = new MyNUnit("../../../../FailedTests");

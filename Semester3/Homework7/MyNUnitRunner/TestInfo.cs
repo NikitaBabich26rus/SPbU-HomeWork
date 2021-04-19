335b464bcca1b5d953bd3050f650040e516d0db2
Nikita26rus <mishamatreshkin@mailru>
2021-04-19 14:11:54 +0300

    - Added project

---
+++ Semester3/Homework7/MyNUnitRunner/TestInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNUnitRunner
+{
+    /// <summary>
+    /// Tests information class
+    /// </summary>
+    public class TestInfo
+    {
+        /// <summary>
+        /// Test result
+        /// </summary>
+        public string Result { get; }
+
+        /// <summary>
+        /// Test name
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Reason of ignore
+        /// </summary>
+        public string IgnoreReason { get; }
+
+        /// <summary>
+        /// Test run time
+        /// </summary>
+        public long Time { get; }
+
+        /// <summary>
+        /// Test info constructor
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <param name="result">Result</param>
+        /// <param name="ignoreReason">Reason of ignore</param>
+        /// <param name="time">Time</param>
+        public TestInfo(string name, string result, string ignoreReason, long time)
+        {
+            this.Result = result;
+            this.Name = name;
+            this.IgnoreReason = ignoreReason;
+            this.Time = time;
+        }
+    }
+}
